aa1693045d63ce66438dd6b76d1885413f4f3b0c
Michael Parisi <mjp9zn@virginia.edu>
2016-10-26 14:53:03 -0400

    Added basic navigation links

--- dev/secureshare/users/views.py
+++ dev/secureshare/users/views.py
@@ -14,13 +14,15 @@ def register(request):
     else:
         form = RegisterUserForm()
     form_title = 'Create New User'
+    form_back = '/users/login/'
     form_action = '/users/register/'
-    return render(request, 'users/general_form.html', {'form': form, 'form_title':form_title, 'form_action':form_action})
+    return render(request, 'users/general_form.html', {'form': form, 'form_title':form_title, 'form_back':form_back, 'form_action':form_action})
 
 
 def profile(request):
     template_name = 'users/profile.html'
-    return render(request, template_name)
+    group_set = ''#request.user.group_set.all()
+    return render(request, template_name, {'groups': group_set})
 
 
 def change_password(request):
@@ -34,5 +36,6 @@ def change_password(request):
     else:
         form = ChangePasswordForm(user=request.user, data=request.POST)
     form_title = 'Change password'
+    form_back = '/users/profile/'
     form_action = '/users/change_password/'
-    return render(request, 'users/general_form.html', {'form': form, 'form_title': form_title, 'form_action': form_action})
+    return render(request, 'users/general_form.html', {'form': form, 'form_title': form_title, 'form_back':form_back, 'form_action':form_action})

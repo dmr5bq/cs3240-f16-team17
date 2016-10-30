f255c5995aab1011a27d3e1fc3fc9158348e6ec1
Dominic Ritchey <dmr5bq@virginia.edu>
2016-10-30 18:36:28 -0400

    upload app - highlight to problem with file uploading, have to write the binary to a location. Other minor changes

--- dev/secureshare/upload/forms.py
+++ dev/secureshare/upload/forms.py
@@ -3,4 +3,9 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     title = forms.CharField(max_length=50, min_length=3)
-    file = forms.FileField()
+    files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+
+
+
+
--- dev/secureshare/upload/views.py
+++ dev/secureshare/upload/views.py
@@ -1,25 +1,42 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, loader
-from .models import FileUpload
 from .forms import UploadFileForm
 
+"""
+10/30 DMR - there is some problem with form validation, so the form is not being stored at the moment
+
+Goals:
+    -> handle multiple files
+    -> display progress bars
+    -> handle file storage (filesystem?)
+    -> successful redirect <--- probably has to do with form validation
+    -> handle file encryption mechanism
+    -> add encryption option to the form (button or checkbox?)
+    -> include user information from the session for storage, determining ownership
+
+"""
 
 def index(request):
 
     if request.method == 'POST':
 
-        form = UploadFileForm(request.POST)
+        form = UploadFileForm(request.POST, request.FILES)
 
+        # 10/30 DMR - This fails right now
         if form.is_valid():
 
-            # file is saved
-            file = request.POST['file']
-            title = request.POST['title']
+            form.save()
 
             return HttpResponseRedirect('upload/success') # this will redirect somewhere in the user profile
 
-    # This means that the method == GET and it's before the file has been uploaded
+        else:
+
+            return HttpResponse("Something went wrong with saving your file, please contact the administrator."
+                                + " <a href=\"/upload/\">GO BACK</a>")
+
+    # This means that the method == GET and it's before the file has been uploaded, shows form
     else:
+
         form = UploadFileForm()
 
         return render(request, 'upload/index.html', {'form': form})
@@ -29,3 +46,4 @@ def success(request):
 
     template = loader.get_template('upload/success.html')
     return render(template.render(request))
+

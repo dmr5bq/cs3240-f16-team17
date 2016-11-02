acd28e23aa1414945147c519a5d05aeb60c66f99
Michael Parisi <mjp9zn@virginia.edu>
2016-11-02 14:37:43 -0400

    Added group view, user view, and a list of all groups/users for site managers

--- dev/secureshare/users/urls.py
+++ dev/secureshare/users/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 
-from .views import register, profile, change_password, create_group, join_group
+from .views import *
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'users/login.html'}, name='login'),
@@ -9,7 +9,13 @@ urlpatterns = [
     url(r'^register/$', register, name='register'),
     url(r'^profile/$', profile, name='profile'),
     url(r'^change_password/$', change_password, name='change-password'),
-    url(r'^create_group/$', create_group, name='create_group'),
-    url(r'^join_group/$', join_group, name='join_group')
 
+    url(r'^create_group/$', create_group, name='create_group'),
+    url(r'^join_group/$', join_group, name='join_group'),
+
+    url(r'^all_users/$', all_users, name='all_users'),
+    url(r'^all_groups/$', all_groups, name='all_groups'),
+
+    url(r'^user/(?P<user_id>[0-9]+)/$', view_user, name='user'),
+    url(r'^group/(?P<group_id>[0-9]+)/$', view_group, name='group'),
 ]
--- dev/secureshare/users/views.py
+++ dev/secureshare/users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import update_session_auth_hash
 
 from .forms import RegisterUserForm, ChangePasswordForm, CreateGroupForm, JoinGroupForm
+from .models import *
 
 
 def register(request):
@@ -40,6 +41,7 @@ def change_password(request):
     form_action = '/users/change_password/'
     return render(request, 'users/general_form.html', {'form': form, 'form_title': form_title, 'form_back':form_back, 'form_action':form_action})
 
+
 def join_group(request):
     if request.method == 'POST':
         form = JoinGroupForm(user=request.user, data=request.POST)
@@ -56,6 +58,7 @@ def join_group(request):
     return render(request, 'users/general_form.html',
                   {'form': form, 'form_title': form_title, 'form_back': form_back, 'form_action': form_action})
 
+
 def create_group(request):
     if request.method == 'POST':
         form = CreateGroupForm(request.POST)
@@ -68,3 +71,47 @@ def create_group(request):
     form_back = '/users/profile/'
     form_action = '/users/create_group/'
     return render(request, 'users/general_form.html', {'form':form, 'form_title':form_title, 'form_back':form_back,'form_action':form_action})
+
+
+def view_group(request, group_id):
+    template_name = 'users/view_group.html'
+    group = get_object_or_404(Group, pk=group_id)
+
+    context = {}
+    context['group'] = group
+    users = []
+    for user in User.objects.all():
+        if group in user.groups.all():
+            users.append(user)
+    context['users'] = users
+
+    return render(request, template_name, context)
+
+
+def view_user(request, user_id):
+    template_name = 'users/view_user.html'
+    user = get_object_or_404(User, pk=user_id)
+
+    context = {}
+    context['user'] = user
+    context['groups'] = user.groups.all()
+
+    return render(request, template_name, context)
+
+
+def all_groups(request):
+    template_name = 'users/all_groups.html'
+
+    context = {}
+    context['groups'] = Group.objects.all()
+
+    return render(request, template_name, context)
+
+
+def all_users(request):
+    template_name = 'users/all_users.html'
+
+    context = {}
+    context['users'] = User.objects.all()
+
+    return render(request, template_name, context)

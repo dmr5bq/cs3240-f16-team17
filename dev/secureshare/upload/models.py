99067c0c9ceec64abee0fab5c800a664acfee40c
Dominic Ritchey <dmr5bq@virginia.edu>
2016-11-02 20:30:01 -0400

    added Report model to upload application

--- dev/secureshare/upload/models.py
+++ dev/secureshare/upload/models.py
@@ -5,8 +5,16 @@ from datetime import *
 # Create your models here.
 
 class FileUpload(models.Model):
+    upload_id = models.AutoField(primary_key=True)
     title = models.TextField(default='title')
     file = models.FileField(default=None)
     timestamp = models.DateTimeField(default=datetime.now())
 
 
+class Report(models.Model):
+    report_id = models.AutoField(primary_key=True)
+    title = models.TextField(default="None")
+    encrypted = models.BooleanField(default=False)
+    file = models.BinaryField(default=bin(0))
+    timestamp = models.DateTimeField(default=datetime.now())
+    owner = models.ForeignKey('users.User', on_delete=models.CASCADE)

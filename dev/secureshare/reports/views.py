d11f1b5bdcc9310013f4f2c5f305f163c1064413
Michael Parisi <mjp9zn@virginia.edu>
2016-11-21 17:25:36 -0500

    fixed file_list in report detail view

--- dev/secureshare/reports/views.py
+++ dev/secureshare/reports/views.py
@@ -123,7 +123,7 @@ class ReportDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(ReportDetailView, self).get_context_data(**kwargs)
-        context['file_list'] = FileUpload.objects.filter(pk=get_object_or_404(Report, pk=self.kwargs['pk']).id)
+        context['file_list'] = FileUpload.objects.filter(report=get_object_or_404(Report, pk=self.kwargs['pk']))
         return context
 
 

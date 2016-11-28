c6675bcb50e8fb32f39d59c350c7e75d09fa8dca
Dominic Ritchey <dmr5bq@virginia.edu>
2016-11-28 16:25:57 -0500

    minor changes to search back-end code

--- dev/secureshare/reports/search.py
+++ dev/secureshare/reports/search.py
@@ -5,10 +5,11 @@ from .models import Report
 from django.shortcuts import render_to_response
 
 
-def normalize(query_string,
+def normalize(query_string, # this method was written based on the method found at http://julienphalip.com/post/2825034077/adding-search-to-a-django-site-in-a-snap
                     findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
                     normspace=re.compile(r'\s{2,}').sub):
     return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)]
+
 
 
 def query(query_string, search_fields):
@@ -17,7 +18,9 @@ def query(query_string, search_fields):
     q_terms = normalize(query_string)
 
     for term in q_terms:
+
         or_q = None
+
         for field_name in search_fields:
             q = Q(**{"%s__icontains" % field_name: term})
             if or_q is None:
@@ -38,10 +41,10 @@ def search( request ):
     if ('q' in request.GET) and request.GET['q'].strip():
         query = request.GET['q']
 
-        entry_query = query(query, ['title', 'body', ])
+        entry_query = query(query, ['title', 'body', ]) # call the query method
 
         found_records = Report.objects.filter(entry_query)
 
         return render_to_response(
-                                  'reports/search.html',
+                                  'reports/search.html', # results page
                                   {'query_string': query, 'found_entries': found_records} )
